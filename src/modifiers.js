a671cba149082f6043bf540ffb39fffea6d9d787
ritz1804 <59580813+ritz1804@users.noreply.github.com>
2021-11-12 17:57:12 +0530

    Update modifiers.js

--- src/modifiers.js
+++ src/modifiers.js
@@ -1,31 +1,25 @@
 export default [
   {
-    // Gros texte
     condition: word => word[0] === '*',
     modify: word => ({ class: 'hero', text: word.substr(1) })
   },
   {
-    // Gros texte en outline
     condition: word => word[0] === '@',
     modify: word => ({ class: 'outline', text: word.substr(1) })
   },
   {
-    // Petit texte
     condition: word => word[0] === '-',
     modify: word => ({ class: 'small', text: word.substr(1) })
   },
   {
-    // Texte barré
     condition: word => word[0] === '~',
     modify: word => ({ class: 'nope', text: word.substr(1) })
   },
   {
-    // Pour faire des slides vides ou échapper un des caractères précédents
     condition: word => word[0] === '.',
     modify: word => ({ class: '', text: word.substr(1) })
   },
   {
-    // Par défaut
     condition: word => true,
     modify: word => ({ class: '', text: word })
   },
